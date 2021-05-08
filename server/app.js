711c3aadf0dba1dbbe28c5e428905906ae4937dc
Gihan S <gihanshp@gmail.com>
2021-05-08 17:56:59 +0530

    Enable cors.

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const photosRouter = require('./routes/photos');
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/photos', photosRouter);
